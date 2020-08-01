5818c6c4d8eeac615b5477e662f0a531b61a1f8c
Scott Lusk <slusk@hitachisolutions.com>
2020-07-31 22:17:02 -0400

    add cosmos configuration

---
+++ shared/Configuration/CosmosDbConfiguration.cs
@@ -0,0 +1,9 @@
+namespace Shared.Configuration
+{
+    public class CosmosDbConfiguration
+    {
+        public string EndpointUri { get; set; }
+        public string PrimarKey { get; set; }
+        public string DatabaseId { get; set; }
+    }
+}
